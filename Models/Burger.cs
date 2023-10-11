840af091bae3c9d9335184e96be9eb44e940aa2f
Nitran2004 <93067795+Nitran2004@users.noreply.github.com>
2023-10-11 08:16:08 -0500

    ingrese atributos en la tabla burger

--- Models/Burger.cs
+++ Models/Burger.cs
@@ -1,6 +1,14 @@
-﻿namespace MartinZumarraga_Ejercicio1CF.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MartinZumarraga_Ejercicio1CF.Models
 {
     public class Burger
     {
+        public int Id { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        public bool WithCheese { get; set; }
+        [Range(0.01, 9999.99)]
+        public decimal Precio { get; set; }
     }
 }
